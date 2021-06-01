0d75cf65345d15d35b8f51ef6fc7efc1217144d9
knmsy1125 <knmsy1125@gmail.com>
2021-06-01 13:02:27 +0000

    rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_13_142128) do
+ActiveRecord::Schema.define(version: 2021_06_01_125808) do
 
   create_table "tasks", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
